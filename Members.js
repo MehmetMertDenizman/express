58ca5cc396877c4f681baec1a432c7975b149f92
Mehmet Mert Denizman <amorylorch@gmail.com>
2022-10-15 20:22:55 +0300

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,13 @@
+const members = [
+    {
+        id: 1,
+        name: "anan"
+    },
+    
+    {
+        id:2,
+        name: "baban"
+    }
+];    
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
  
-
 const app = express();
+
+const logger = require('./middleware/logger')
+
 const PORT = process.env.PORT || 35344;
 
-app.get('/', (req, res) => {res.send('Hello World!');
-});
+
+
+app.use(logger);
+
+app.use('/api/members', require('./routes/api/members'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
